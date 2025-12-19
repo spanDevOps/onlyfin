33cb6261325ebc55d0aaa651deb5ef22eb87320d
Spandan <spandankb@gmail.com>
2025-12-19 09:26:05 +0530

    Resolve error 3

---
+++ types/pdf-parse-fork.d.ts
@@ -0,0 +1,31 @@
+declare module 'pdf-parse-fork' {
+  interface PDFInfo {
+    PDFFormatVersion?: string;
+    IsAcroFormPresent?: boolean;
+    IsXFAPresent?: boolean;
+    Title?: string;
+    Author?: string;
+    Subject?: string;
+    Creator?: string;
+    Producer?: string;
+    CreationDate?: string;
+    ModDate?: string;
+  }
+
+  interface PDFMetadata {
+    _metadata?: any;
+  }
+
+  interface PDFData {
+    numpages: number;
+    numrender: number;
+    info: PDFInfo;
+    metadata: PDFMetadata;
+    text: string;
+    version: string;
+  }
+
+  function pdfParse(dataBuffer: Buffer): Promise<PDFData>;
+
+  export default pdfParse;
+}
